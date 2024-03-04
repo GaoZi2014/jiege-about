50974c951107744ac98e90e99f7c62ac8bdca52d
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2024-03-04 10:29:38 +0800

    Update script.js

--- script.js
+++ script.js
@@ -6,12 +6,3 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'none'
 }
-if (navigator.userAgent.match(/MSIE 8/)) {
-  window.location.href = "browser_not_support.html";
-}
-if (navigator.userAgent.match(/MSIE 9/)) {
-  window.location.href = "browser_not_support.html";
-}
-if (navigator.userAgent.match(/MSIE 10/)) {
-  window.location.href = "browser_not_support.html";
-}

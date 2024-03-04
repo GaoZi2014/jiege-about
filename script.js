6362866f18d8708b6d879a4029b4347939819524
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2024-03-04 09:55:58 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -6,3 +6,6 @@ function hideSidebar(){
   const sidebar = document.querySelector('.sidebar')
   sidebar.style.display = 'none'
 }
+if (navigator.userAgent.match(/MSIE 8/)) {
+  window.location.href = "browser_not_support.html";
+}

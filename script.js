8f8ee0a6bafd06a669da196ffd3dc944eb1ea8f9
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2024-06-20 20:07:13 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function showSidebar() {
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'flex'
+  window.scrollTo(0,0);
+  setTimeout(
+    function showSidebar2(){
+      const sidebar = document.querySelector('.sidebar');
+      sidebar.style.display = 'flex';
+    }
+  , 500)
 }
 
 function hideSidebar() {

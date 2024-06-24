94cf0937aaa6308f099e43d938b2b102902f4f91
Jiege <122613317+GaoZi2014@users.noreply.github.com>
2024-06-24 18:08:25 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,16 +1,22 @@
 function showSidebar() {
+  const sidebar = document.querySelector('.sidebar');
   window.scrollTo(0,0);
+  sidebar.style.display = 'flex';
   setTimeout(
     function showSidebar2(){
-      const sidebar = document.querySelector('.sidebar');
-      sidebar.style.display = 'flex';
+      sidebar.style.right = '0px';
     }
-  , 500)
+  , 200)
 }
 
 function hideSidebar() {
-  const sidebar = document.querySelector('.sidebar')
-  sidebar.style.display = 'none'
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.right = '-500px';
+  setTimeout(
+    function showSidebar2(){
+      sidebar.style.display = 'none';
+    }
+  , 200)
 }
 
 function downloadFile() {
@@ -29,8 +35,10 @@ window.onscroll = function() {
   let navbar = document.querySelector('nav');
   if (prevScrollpos > currentScrollPos) {
     navbar.style.top = '0';
+    navbar.style.boxShadow = '5px 5px 5px #00000018';
   } else {
     navbar.style.top = '-50px';
+    navbar.style.boxShadow = '0px 0px 0px #00000018';
   }
   prevScrollpos = currentScrollPos;
 };
